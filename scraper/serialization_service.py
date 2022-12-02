848a03a0078591c9b3e19ed930169114e413a802
saxomoose <mathieu.tulpinck@gmail.com>
2022-12-02 09:33:13 +0000

    Modify json structure

--- scraper/serialization_service.py
+++ scraper/serialization_service.py
@@ -1,7 +1,8 @@
 import dataclasses
 import json
+import pprint
 
-from scraper import utils
+from scraper import models, utils
 
 logger = utils.get_logger(__name__)
 
@@ -9,7 +10,22 @@ logger = utils.get_logger(__name__)
 def to_json(enterprise_number, parents, children):
     json_dict = dict()
     json_list = list()
-    for parent_key, parent_value in parents.items():
+
+    for parent_key, parent_value in parents.copy().items():
+        if isinstance(parent_value, models.Entity):
+            parent_dataclass_asdict = dataclasses.asdict(parent_value)
+            child_dataclass_asdict = dataclasses.asdict(children[parent_key])
+            parent_dataclass_asdict.update(child_dataclass_asdict)
+            for child_key, child_value in children.copy().items():
+                child_d_a = dataclasses.asdict(child_value)
+                parent_d_a = dataclasses.asdict(parents[child_key])
+                child_d_a.update(parent_d_a)
+                if isinstance(child_value, models.EntityEntity):
+                    if child_value.representative_entity == parent_key:
+                        del child_d_a["representative_entity"]
+                        parent_dataclass_asdict["permanent_representative"] = child_d_a
+                        json_list.append(parent_dataclass_asdict)
+
         parent_dataclass_asdict = dataclasses.asdict(parent_value)
         child_dataclass_asdict = dataclasses.asdict(children[parent_key])
         parent_dataclass_asdict.update(child_dataclass_asdict)
